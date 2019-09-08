96e6556c6bcd2645e6814e1f2c5e9782563873f2
alex93skr <49789700+alex93skr@users.noreply.github.com>
2019-09-08 22:14:13 +0300

    1

--- ru_chp.py
+++ ru_chp.py
@@ -135,8 +135,8 @@ def index():
 #############################################################
 
 if __name__ == "__main__":
-    app.run()
-    # app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
+    # app.run()
+    app.run(host="0.0.0.0", port=os.environ.get('PORT', 5000))
     # app.run(debug=True)
 
 #############################################################
